a03a4ec5bd0e27811cbb828066a1d235af5eabb0
Erick Renaud <renaud_junior@outlook.com>
2024-08-29 01:48:00 -0400

    Added comment model

---
+++ models/comment.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model {};
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    authorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    publishedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+})
+
+module.exports = Comment;
