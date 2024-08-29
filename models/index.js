ed4be75babd7ce0d650a8cce54233e7b93600d34
Erick Renaud <renaud_junior@outlook.com>
2024-08-29 01:49:13 -0400

    Added assosiations

--- models/index.js
+++ models/index.js
@@ -2,4 +2,10 @@ const User = require('./user');
 const Post = require('./post');
 const Comment = require('./comment');
 
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
 module.exports = { User, Post, Comment };
